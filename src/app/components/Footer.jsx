45b4fb7ff560baaec341c818afb348b9e31357b3
Win0011 <winlife1101@gmail.com>
2023-11-20 16:08:23 +0100

    fix modify

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Footer() {
+  return (
+   <footer className="bg-[gold] flex items-center justify-center h-[20vh]">
+    <p>&copy; univelcity {new Date().getFullYear()}</p>
+    
+
+
+   </footer>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+
+export default function page() {
+  return (
+    <main>
+      <h1>Hello React</h1>
+      <section>
+        <h2>Hello Next</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad iusto nesciunt exercitationem quis, quod ullam at ipsa quidem saepe, tenetur dicta iste accusantium et eos porro placeat itaque aliquid blanditiis?</p>
+      </section>
+    </main>
+    
+  )
+}
